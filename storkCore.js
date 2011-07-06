c0bfdd72625e99f4d576c8d04f9f3c99db4a1582
Kristoffer Lawson <setok@scred.com>
2011-07-06 17:10:29 +0300

    Added IE fallback to transition event handler.

--- storkCore.js
+++ storkCore.js
@@ -31,6 +31,28 @@
 var includedNum = 0;
 
 
+var supports = (function() {  
+   var div = document.createElement('div'),  
+      vendors = 'Khtml Ms O Moz Webkit'.split(' '),  
+      len = vendors.length;  
+  
+   return function(prop) {  
+      if ( prop in div.style ) return true;  
+  
+      prop = prop.replace(/^[a-z]/, function(val) {  
+         return val.toUpperCase();  
+      });  
+  
+      while(len--) {  
+         if ( vendors[len] + prop in div.style ) {  
+            return true;  
+         }  
+      }  
+      return false;  
+   };  
+})();  
+
+
 function addLoadEvent(func) { 
     var oldonload = window.onload; 
     if (typeof window.onload != 'function') { 
