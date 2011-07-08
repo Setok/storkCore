26ae53fc1963117a1c9483c7c255aa0511e70a56
Kristoffer Lawson <setok@scred.com>
2011-07-08 17:25:27 +0300

    Fixed vendor option check. After third call would run forever.

--- storkCore.js
+++ storkCore.js
@@ -32,23 +32,23 @@ var includedNum = 0;
 
 
 var cssSupports = (function() {  
-   var div = document.createElement('div'),  
-      vendors = 'Khtml Ms O Moz Webkit'.split(' '),  
-      len = vendors.length;  
+    var div = document.createElement('div');
+    var vendors = 'Khtml Ms O Moz Webkit'.split(' ');
   
    return function(prop) {  
-      if ( prop in div.style ) return true;  
+       var len = vendors.length;  
+       if ( prop in div.style ) return true;  
   
-      prop = prop.replace(/^[a-z]/, function(val) {  
-         return val.toUpperCase();  
-      });  
-  
-      while(len--) {  
-         if ( vendors[len] + prop in div.style ) {  
-            return true;  
-         }  
-      }  
-      return false;  
+       prop = prop.replace(/^[a-z]/, function(val) {  
+           return val.toUpperCase();  
+       });  
+       
+       while(len--) {  
+           if ( vendors[len] + prop in div.style ) {  
+               return true;  
+           }  
+       }  
+       return false;  
    };  
 })();  
 
